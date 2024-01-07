a9ef75b8df2f295ef768c20b71ac6f5529bed559
mohamadjavadakef <moha.javad83.90@gmail.com>
2024-01-07 13:14:26 -0800

    add menus and faqs

---
+++ database/migrations/2024_01_07_210320_create_faqs_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faqs_categories', function (Blueprint $table) {
+            $table->id();
+            $table->text('question');
+            $table->text('answer');
+            $table->string('sluq')->unique()->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->string('tags');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faqs_categories');
+    }
+};
