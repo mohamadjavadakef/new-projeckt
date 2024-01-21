b8e00bbf76f27801fd23566bd747c5df9748d4d6
mohamadjavadakef <moha.javad83.90@gmail.com>
2024-01-21 04:57:59 -0800

    slug

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -2,13 +2,24 @@
 
 namespace App\Models\Content;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostCategory extends Model
 {
-    use HasFactory , SoftDeletes;
+    use HasFactory , SoftDeletes , Sluggable;
+
+    public function Sluggable() : array {
+
+        return[
+            'sluq' =>[
+                'source' => 'name'
+            ]
+
+        ];
+    }
 
     protected $fillable=['name','description','sluq','image','status','tags'];
 }
